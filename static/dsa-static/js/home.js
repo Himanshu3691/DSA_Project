84f2ab6c4e71069753bf48aaec917de7a857749f
Mayank Partap Singh <85053432+mayank2269@users.noreply.github.com>
2024-02-29 21:27:40 +0530

    new ui update by maya 22

--- static/dsa-static/js/home.js
+++ static/dsa-static/js/home.js
@@ -18,15 +18,39 @@
 
 
   register_txt = document.getElementById('register-new-user');
-  document.getElementById('register-new-user').addEventListener('click', function() {
-    document.getElementById('add-users').style.display = 'block';
-    register_txt.style.display = 'none'
-   
-  });
-
-  document.getElementById('close-popup').addEventListener('click', function() {
-    document.getElementById('add-users').style.display = 'none';
-    register_txt.style.display = 'block'
-  });
+ 
 
 
+
+document.getElementById('register-new-user').addEventListener('click', function() {
+  document.getElementById('add-users').style.display = 'block';
+  register_txt.style.display = 'none'
+});
+
+document.getElementById('close-popup').addEventListener('click', function() {
+  document.getElementById('add-users').style.display = 'none';
+});
+
+document.getElementById('toggle-link').addEventListener('click', function(event) {
+  event.preventDefault();
+  var signupContainer = document.querySelector('.signup-container');
+  var loginContainer = document.querySelector('.login-container');
+
+  if (signupContainer.style.display === 'none') {
+    signupContainer.style.display = 'block';
+    loginContainer.style.display = 'none';
+    document.getElementById('popup-title').innerHTML = '<h3>Sign Up</h3>';
+    document.getElementById('toggle-link').innerHTML = 'Login';
+  } else {
+    signupContainer.style.display = 'none';
+    loginContainer.style.display = 'block';
+    document.getElementById('popup-title').innerHTML = '<h3>Login</h3>';
+    document.getElementById('toggle-link').innerHTML = 'Sign Up';
+  }
+});
+
+// Trigger the sign-in pop-up when the page is opened
+window.onload = function() {
+  document.querySelector('.signup-container').style.display = 'none';
+  document.getElementById('add-users').style.display = 'block';
+};
